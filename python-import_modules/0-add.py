6e2632a364ce552b2f35791366e98c7647984dd7
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2022-10-19 18:18:41 +0200

    fff

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
-a = 1
-b = 2
+from add_0.py import add
 
 if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
