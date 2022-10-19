4ab985b524bbb3b38354f63be2aadaa327989c7f
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2022-10-19 21:15:03 +0200

    ttt

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-
-if __name__ == '__main__':
-    args_number = len(argv) - 1
-
-    if args_number > 0:
-        sum_ = 0
-        for args in argv[1:]:
-            sum_ += int(args)
-        print("{}".format(sum_))
-    else:
-        print("{}".format(args_number))
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
