d651ed6e707ac8a4d331293710347c70a8d205ab
Erica Nshimirimana <e.nshimirim@alustudent.com>
2022-12-03 18:37:37 +0200

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-# 0-add_integer.py
-"""Defines an integer addition function."""
-
-
+"""Defines a function addition of integers"""
 def add_integer(a, b=98):
-    """Return the integer addition of a and b.
+    """Return an integer: the addition of a and b.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
