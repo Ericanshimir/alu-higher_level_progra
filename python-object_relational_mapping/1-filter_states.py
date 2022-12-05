a38fa04418eb041c5dfb01adaa588ca8b0f745a8
Erica Nshimirimana <e.nshimirim@alustudent.com>
2022-12-05 12:45:28 +0200

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" module list states
-from database"""
+""" Write a script that lists all states with a name starting with N"""
 
 if __name__ == "__main__":
     import MySQLdb
