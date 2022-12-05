969ef5f06d0b2d668ddeb0b5cc748856a246feb7
Erica Nshimirimana <e.nshimirim@alustudent.com>
2022-12-05 12:48:55 +0200

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" module list states
-from database"""
+""" Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
 
 if __name__ == "__main__":
     import MySQLdb
