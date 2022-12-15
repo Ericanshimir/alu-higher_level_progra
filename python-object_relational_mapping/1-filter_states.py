adc6d43fb502e8334488aeb9d27a4b35bbb0c987
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2022-12-15 20:31:17 +0200

    Update 1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that lists all states with a name starting with N"""
+""" A script that lists all states with a name starting with N"""
 
 if __name__ == "__main__":
     import MySQLdb
