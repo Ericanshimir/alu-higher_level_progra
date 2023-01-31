9c22cc8995c3a9db4ff618a47b41aba1955d5424
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2023-01-31 20:17:30 +0200

    Update 6-square.js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const MainSquare = require('./5-square');
 
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+module.exports = class Square extends MainSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let row = 1; row <= this.height; row++) {
+        console.log(c.repeat(this.width));
+      }
+    }
   }
 };
