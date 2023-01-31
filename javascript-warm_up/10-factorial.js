e44f3ad5faa92aaf60809249d3e7011910a92efb
Erica <e.nshimirim@alustudent.com>
2023-01-31 08:28:13 -0800

    fact

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,9 +3,9 @@
 const num = parseInt(process.argv[2], 10);
 
 function factorial (n) {
-	    if (isNaN(n) || n === 0) {
-		    	return (1);
+  if (isNaN(n) || n === 0) {
+    return (1);
 		        }
-	    return (n * factorial(n - 1));
-}
+    return (n * factorial(n - 1));
+  }
 console.log(factorial(num));
