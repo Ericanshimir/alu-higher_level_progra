31cb9c636acaaecbed382d95db6bd6a0ac490450
Erica <e.nshimirim@alustudent.com>
2023-01-31 04:05:31 -0800

    task8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const side = parseInt(process.argv[2], 10);
 
 if (isNaN(side)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 1; i <= side; i++) {
-	console.log('X'.repeat(side));
+   for (let i = 1; i <= side; i++) {
+  console.log('X'.repeat(side));
     }
 }
