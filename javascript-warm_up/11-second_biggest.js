3f155b59acddf0dd5c20f4e67bedf39485dbdebd
Erica <e.nshimirim@alustudent.com>
2023-01-31 08:34:33 -0800

    11-decond

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,9 @@
 const argArray = process.argv.slice(2);
 
 if (argArray.length < 2) {
-    console.log(0);
+  console.log(0);
 } else {
-    argArray.sort((a, b) => (a - b));
-    argArray.pop();
-    console.log(argArray.pop());
+  argArray.sort((a, b) => (a - b));
+  argArray.pop();
+  console.log(argArray.pop());
 }
