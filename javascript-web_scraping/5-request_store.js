9c2344e131c6d9dfefb6364a55194e6135f306d8
Ericanshimir <109414922+Ericanshimir@users.noreply.github.com>
2023-02-02 21:09:51 +0200

    Update 5-request_store.js

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,23 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
-
+const fs = require('fs');
 const path = process.argv[2];
+const file = process.argv[3];
 
 request(path, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    let count = 0;
-    const result = JSON.parse(body).results;
-    for (const item in result) {
-      const charac = result[item].characters;
-      for (const i in charac) {
-        if (charac[i].includes('18')) {
-          count++;
-        }
+    fs.writeFile(file, body, 'utf-8', function (error) {
+      if (error) {
+        console.error(error);
       }
-    }
-    console.log(count);
+    });
   }
 });
